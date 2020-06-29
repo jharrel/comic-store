2f6119bb0e71b7fa8ff59aff37813a12949248e9
Jason Harrel <harrel.jason@gmail.com>
2020-06-28 19:26:23 -0700

    Added and styled shop page.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const ComicsPage = () => (
-  <div>
-    <h1>Comics Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={ComicsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,13 +11,13 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: 'comics',
+          title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'comics'
+          linkUrl: 'hats'
         },
         {
-          title: 'supplies',
+          title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
           linkUrl: ''
@@ -29,14 +29,14 @@ class Directory extends React.Component {
           linkUrl: ''
         },
         {
-          title: 'comics',
+          title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
           linkUrl: ''
         },
         {
-          title: 'supplies',
+          title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
